f64238f224bc1dac782fd9f95f0f499f118b3808
Juan Bernardo Benavides Rubio <JuanBer@Juans-MacBook-Air-3.local>
2019-08-21 17:58:28 -0500

    Fire bullets
    The ship fires bullets with spacebar and they are deleted after they
    leave the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -14,11 +15,20 @@ def run_game():
 
     # Make a ship
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                 bullets.remove(bullet)
+
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
